eaeec736b99a837eeb71682926ebbe4993f2babc
tomonaganoriaki <115410371+tomonaganoriaki@users.noreply.github.com>
2022-11-06 13:58:41 +0900

    Update memo.rb

--- memo.rb
+++ memo.rb
@@ -1,42 +1,30 @@
 require "csv"
 
-#最初ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
-
 puts "新規メモを作成する → 1\r\n既存メモを編集する → 2"
-  memo_type = gets.chomp.to_i
- 
-#選択肢１ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+memo_type = gets.chomp.to_i
 
-  if memo_type == 1
-  puts "新規でメモを作成します。拡張子を除いたファイル名を入力してください。"
-  
-  file_name = gets.chomp
-  
-  puts "メモの内容を記入して下さい。Ctrl+Dで保存します。"
-  
-  memo = STDIN.read
-  
-  CSV.open("#{file_name}.csv","w") do |csv|
-   csv << ["#{memo}"]
-  end
- 
- #選択肢２ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
- 
-  elsif memo_type == 2
-  puts "既存のメモを編集します。拡張子を除いた既存ファイル名を入力してください。"
-　file_name = gets.chomp
-　
-　puts "メモの内容を記入して下さい。Ctrl+Dで保存します。"
-　
-　memo = STDIN.read
-　
-　CSV.open("#{file_name}.csv","a") do |csv|
-    csv << ["#{memo}"]
-  　end
-  end
-  
- #その他ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
- 
-  else
-  puts "1か2の数字を入力してください"
-  end
+if memo_type == 1
+puts "新規でメモを作成します。拡張子を除いたファイル名を入力してください。"
+
+file_name = gets.chomp
+
+puts "メモの内容を記入して下さい。Ctrl+Dで保存します。"
+
+memo = STDIN.read
+
+CSV.open("#{file_name}.csv","w") do |csv|
+csv << ["#{memo}"]
+end
+
+elsif memo_type == 2
+puts "既存のメモを編集します。拡張子を除いた既存ファイル名を入力してください。"
+file_name = gets.chomp
+
+puts "メモの内容を記入して下さい。Ctrl+Dで保存します。"
+
+memo = STDIN.read
+
+CSV.open("#{file_name}.csv","a") do |csv|
+csv << ["#{memo}"]
+end
+end
